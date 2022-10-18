02fbc43383f85ab4c7c6052deae85cb6fffaf302
Sweta <srialwarnaidu@gmail.com>
2022-10-19 00:55:27 +0530

    second commit

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -8,37 +8,43 @@ export default function Meme(){
                          bottomText:"",
                          randomImg:"https://i.imgflip.com/30b1gx.jpg"
     })
+    const[allmemes,setallmemes] = React.useState([])
+    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+         .then(res=>res.json())
+         .then(data=>setallmemes(data.data.memes))
+    },[])
+
     function handleClick(){
-        const memeArray= memeData.data.memes;
-        const randomNumber = Math.floor(Math.random()*memeArray.length);
+        const randomNumber = Math.floor(Math.random()*allmemes.length);
         setMeme(prevMeme=>{
             return({
                 ...prevMeme,
-                randomImg:memeArray[randomNumber].url
+                randomImg:allmemes[randomNumber].url
             })
         }) 
     } 
     function handleChange(event){
         const {name,value}=event.target;
-        console.log(name,value)
         setMeme(prevMeme=>({
             ...prevMeme,
             [name]:value
         }))
+
     }
     return(
         <div>
           <div className="form">
             <form className="form-input">
-               <input type="text" name="top-text" value={meme.topText} onChange={handleChange} placeholder="Top Text"/>
-               <input type="text" name="bottom-text" value={meme.bottomText} onChange={handleChange} placeholder="Bottom Text"/>
+               <input type="text" name="topText"  onChange={handleChange} placeholder="Top Text"/>
+               <input type="text" name="bottomText" onChange={handleChange} placeholder="Bottom Text"/>
             </form>
             <button className="submit" onClick={handleClick}>Get New Meme Image</button>
           </div>
           <div className="meme">
              <img src={meme.randomImg} alt="meme image" className="meme-image"/>
-                <h2 className="meme-top-text">hi</h2>
-                <h2 className="meme-bottom-text">Bye</h2>
+             <h2 className="meme-top-text">{meme.topText}</h2>
+             <h2 className="meme-bottom-text">{meme.bottomText}</h2>
           </div>
 
         </div>
